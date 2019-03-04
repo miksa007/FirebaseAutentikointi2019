1d405516c9a06576439a6686206a4bb8f84da4c5
Mika Saari <mika.saari@tut.fi>
2019-03-04 11:01:57 +0200

    Osa 1 toimii

---
+++ Autentikointi/app/src/main/java/fi/tuni/miksa/autentikointi2019/MainActivity.java
@@ -0,0 +1,117 @@
+package fi.tuni.miksa.autentikointi2019;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    //osa 1
+    private FirebaseAuth mAuth;
+    private static final int RC_SIGN_IN = 123;
+    private final String TAG = "softa";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //osa 1
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+    //osa1
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    //osa 1
+    public void kirjauduSisaan(View view) {
+        Log.d(TAG, "kirjaudu nappulaa painettu");
+        createSignInIntent();
+
+    }
+
+    //osa 1
+    public void kirjauduUlos(View view) {
+        Log.d(TAG, "kirjaudu ulos nappulaa painettu");
+        signOut();
+
+    }
+
+    //osa 1
+    public void createSignInIntent() {
+        Log.d(TAG, "autetikointia alkoi");
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build());
+//                new AuthUI.IdpConfig.PhoneBuilder().build(),
+//                new AuthUI.IdpConfig.GoogleBuilder().build(),
+//                new AuthUI.IdpConfig.FacebookBuilder().build(),
+//                new AuthUI.IdpConfig.TwitterBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+        // [END auth_fui_create_intent]
+    }
+
+    //osa 1
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "resulttia pukkaa");
+        if (requestCode == RC_SIGN_IN) {
+            Log.d(TAG, "resulttia pukkaa RC_SIGN_IN ok");
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                Log.d(TAG, "resulttia pukkaa RESULT ok");
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                Log.d(TAG, "resulttia pukkaa - ei onnistunut");
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+    //osa 1
+    public void signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                        Log.d(TAG, "uloskirjauduttu");
+                    }
+                });
+        // [END auth_fui_signout]
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # FirebaseAutentikointi2019
 Tutoriaali autentikoinnin lisäykseen
+
+##Oheismateriaali ja muut huomiot
+* [https://firebase.google.com/docs/auth/android/password-auth](https://firebase.google.com/docs/auth/android/password-auth)
+* Repo sisältää vain muutettavia koodi -tiedostoja, joten ei ole ajettavissa suoraan
+* Tässä luetellaan eri työvaiheet seuraten dokumentaatiota
+
+
+##Osa 1
+* Aloitetaan uusi empty project Android Studiolla
+* Lisätään projekki Firebaseen, vanhojen oppien mukaan tai seuraten ohjetta:[https://firebase.google.com/docs/android/setup](https://firebase.google.com/docs/android/setup) 
+* Lisätään google-services.json tiedosto mukaan android studio -projektiin
+* Build.gradle -tiedostojen muokkaus (Firebase ohjeen mukaan)
+* Project-level build.gradle (<project>/build.gradle):
+```python
+    classpath 'com.google.gms:google-services:4.0.1' 
+```
+* App-level build.gradle (<project>/<app-module>/build.gradle):
+```python 
+    implementation 'com.google.firebase:firebase-core:16.0.1'
+    implementation 'com.firebaseui:firebase-ui-auth:4.3.1'
+    ...
+    apply plugin: 'com.google.gms.google-services'
+```
+* Huomioi yllä: implementation 'com.firebaseui:firebase-ui-auth:4.3.1'
+* Lisää komponentit activity_main.xml tiedostoon (3 nappulaa).
+* seuraillaan oheismateriaalin vaiheita ja lisäillään koodia MainActivity.java tiedostoon.
+* Kaikki //Osa 1 -kommentilla mukaan omaan sovellukseen
+* **Valmis testaukseen**
+* LOG IN -nappula kysyy sähköpostiosoitteen, nimen ja salasanan. Ja kirjautuu Firebaseen.
+* Jos kaikki meni oikein niin [https://console.firebase.google.com](https://console.firebase.google.com) Authentication ja Users. Listassa pitäisi näkyä antamasi tiedot...
+* Jos ei näy - niin LogCat:stä etsimään virhettä.
